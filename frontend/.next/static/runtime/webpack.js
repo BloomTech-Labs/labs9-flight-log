10443c0b5afcecd0021d439e0b2442ecffab8781
unknown <quan.nguyen199712@gmail.com>
2019-01-09 13:22:40 -0500

    navigation tweaking

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ae82e605518dc94df71e";
+/******/ 	var hotCurrentHash = "55f4bf5587f597db2af1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
