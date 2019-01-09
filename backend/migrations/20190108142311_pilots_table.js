2a4db783ee9faa0730bf498bcda5ce78c0bb622a
Wanda <doubleuevans@gmail.com>
2019-01-08 18:54:32 -0600

    wrote schema for pilots database

--- backend/migrations/20190108142311_pilots_table.js
+++ backend/migrations/20190108142311_pilots_table.js
@@ -1,8 +1,29 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('pilots', pilots => {
+    pilots
+      .increments()
+      .primary() //primary key
+    //username
+    pilots
+      .string('username', 16)
+      .unique()
+      .notNullable();
+    //isLoggedIn
+    pilots.boolean('isLoggedIn');
+    //isPaid
+    pilots.boolean('isPaid');
+    //password //limited to 16 for the project
+    pilots
+      .string('password', 16)
+      .notNullable();
+
+  }) 
 };
 
 exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('pilots')
   
 };
+
+//commented by w.e.
