24123c64d6b03e1adc2d72e8a0692482cd534e6b
Trevor Lewis <tenzingtitan@gmail.com>
2019-01-09 16:14:35 -0700

    Fixed 'Billing' error 404. Spelling error

--- frontend/components/Navigation.js
+++ frontend/components/Navigation.js
@@ -7,7 +7,7 @@ const Navigation= ()=>{
                 <li><Link href='/Flights'><a>Flights</a></Link></li>
                 <li><Link href='/Aircraft'><a>Aircraft</a></Link></li>
                 <li><Link href='/Instructors'><a>Instructors</a></Link></li>
-                <li><Link href='/Billing'><a>Billing</a></Link></li>
+                <li><Link href='/billing'><a>Billing</a></Link></li>
                 <li><Link href='/Settings'><a>Settings</a></Link></li>
             </ul>
         </div>
