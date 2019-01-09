7b5e732c42759cb673773c1eaa20ac4f4508efba
ssmodish <smodish@hotmail.com>
2019-01-09 12:36:09 -0500

    edited package.json to run a build and export build into a folder

--- frontend/.next/server/static/development/pages/index.js
+++ frontend/.next/server/static/development/pages/index.js
@@ -104,7 +104,7 @@ module.exports =
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-var _jsxFileName = "D:\\projects\\labs9-flight-log\\frontend\\pages\\index.js";
+var _jsxFileName = "C:\\Users\\Steve\\Documents\\Development\\Lambda\\Labs\\labs9-flight-log\\frontend\\pages\\index.js";
 
 
 var Index = function Index() {
