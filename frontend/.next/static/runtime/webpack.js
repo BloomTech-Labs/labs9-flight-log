c958c4e11df7b02822776140d31dda2c8e35964b
unknown <quan.nguyen199712@gmail.com>
2019-01-09 13:39:52 -0500

    added .next to gitignore

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "55f4bf5587f597db2af1";
+/******/ 	var hotCurrentHash = "19873b69408109015541";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
