5b8dbd4246f5e8156fd2973f6a0f6aa54ef41db8
Wanda <doubleuevans@gmail.com>
2019-01-09 16:55:53 -0600

    configured for aws db usage; add .unsigned to instr, flight, aircraft

--- backend/migrations/20190108142332_aircrafts_table.js
+++ backend/migrations/20190108142332_aircrafts_table.js
@@ -1,14 +1,17 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('aircrafts', aircrafts => {
-    aircrafts.string('make')
-    aircrafts.string('model')
-    aircrafts.string('tailNumber')
-    aircrafts.integer('flightID').references('flights.id')
-  })
-  
-};
+  knex.schema.dropTableIfExists('aircrafts').then(() => { 
+    return knex.schema.createTable('aircrafts', aircrafts => {
+      aircrafts.string('make')
+      aircrafts.string('model')
+      aircrafts.string('tailNumber')
+      aircrafts
+        .integer('flightID')
+        .unsigned()
+        .references('flights.id')
+    })
+})};
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('flights')
+  return knex.schema.dropTableIfExists('aircrafts')
 };
--- backend/migrations/20190108142432_instructors_table.js
+++ backend/migrations/20190108142432_instructors_table.js
@@ -1,14 +1,19 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('instructors', instructors => {
+  knex.schema.dropTableIfExists('instructors').then(() => {
+    return knex.schema.createTable('instructors', instructors => {
     instructors.string('name').notNullable()
     instructors.string('licenseNum', 14).notNullable() 
     instructors.text('notes', 512)
     instructors.text('ratings', 256)
     instructors.text('contactInfo', 512)
-    instructors.integer('flightID').references('flights.id')
+    instructors
+      .integer('flightID')
+      .unsigned()
+      .references('flights.id')
   })
-};
+}
+)};
 
 exports.down = function(knex, Promise) {
   return knex.schema.dropTableIfExists('instructors')
