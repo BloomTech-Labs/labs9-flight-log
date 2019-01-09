e927d231c736ed85e72df5e67cf4f5fb6cc28819
ssmodish <smodish@hotmail.com>
2019-01-09 16:35:41 -0500

    committing all in order to merge branches

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "30461a802b5e340d3063";
+/******/ 	var hotCurrentHash = "e3ea320a68414655ab07";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
