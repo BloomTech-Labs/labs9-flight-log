179153fa66084cd9c04fe3c52d7c1a4a46ebbbab
ssmodish <smodish@hotmail.com>
2019-01-09 19:19:38 -0500

    zeit deploy

--- frontend/out/_next/static/development/pages/_app.js
+++ frontend/out/_next/static/development/pages/_app.js
@@ -9117,7 +9117,11 @@ module.exports = function(module) {
 /***/ (function(module, exports, __webpack_require__) {
 
 __NEXT_REGISTER_PAGE('/_app', function() {
+<<<<<<< HEAD
 module.exports = __webpack_require__(/*! C:\Users\Steve\Documents\Development\Lambda\Labs\labs9-flight-log\frontend\node_modules\next\dist\pages\_app.js */"./node_modules/next/dist/pages/_app.js");
+=======
+module.exports = __webpack_require__(/*! C:\Users\Quan\Desktop\labs9-flight-log\frontend\node_modules\next\dist\pages\_app.js */"./node_modules/next/dist/pages/_app.js");
+>>>>>>> 2768f3c32e6a49ab471e261d974c152ee789b788
 
 return { page: module.exports.default }});
 
