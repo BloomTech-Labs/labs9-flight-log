0b3d1a8e3b829309c91f25a45768106d69b19ca5
jeffyjkang <jeffyjkang@gmail.com>
2019-01-10 11:00:30 -0800

    update basic routes for pilots, add id on aircraft and instructors

---
+++ backend/helpers/pilotsDb.js
@@ -0,0 +1,14 @@
+// const pilotsDb = require("../dbConfig");
+
+// module.exports = {
+//   get: () => pilotsDb("pilots"),
+//   insert: pilot => pilotsDb("pilots").insert(pilot),
+//   update: (id, pilot) =>
+//     pilotsDb("pilots")
+//       .where("id", id)
+//       .update(pilot),
+//   remove: id =>
+//     pilotsDb("pilots")
+//       .where("id", id)
+//       .del()
+// };
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,15 @@
 const express = require("express");
 const server = express();
 
+//import routes
+// const pilots = require("./routes/pilotsRoute");
+
 server.use(express.json());
 
 server.get("/", (req, res) => {
   res.send("<h1>Server running</h1>");
 });
+
+// server.use("/pilots", pilots);
 
 server.listen(9000, () => console.log("api running..."));
