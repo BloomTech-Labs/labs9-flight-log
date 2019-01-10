e508a4727c75202714d82b9268c912b6f0e3f43b
Trevor Lewis <tenzingtitan@gmail.com>
2019-01-10 13:43:25 -0700

    Navigation/Flights/Layout has been modified for basic styling

--- frontend/components/Navigation.js
+++ frontend/components/Navigation.js
@@ -1,16 +1,72 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Link from 'next/link';
-const Navigation= ()=>{
-    return(
-        <div>
-            <ul>
-                <li><Link href='/'><a>Home</a></Link></li>
-                <li><Link href='/Flights'><a>Flights</a></Link></li>
-                <li><Link href='/Aircraft'><a>Aircraft</a></Link></li>
-                <li><Link href='/Instructors'><a>Instructors</a></Link></li>
-                <li><Link href='/billing'><a>Billing</a></Link></li>
-                <li><Link href='/Settings'><a>Settings</a></Link></li>
-            </ul>
-        </div>
-    )
+import MenuList from '@material-ui/core/MenuList';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const styles = theme => ({
+  root: {
+      display: 'flex',
+      flexDirection: 'column',
+    //   width: "20%",
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+  button: {
+    margin: theme.spacing.unit,
+    width: '90%',
+  },
+  input: {
+    display: 'none',
+  },
+});
+
+function Navigation(props) {
+  const { classes } = props;
+
+  return (
+    <div>
+      <MenuList className={classes.root} elevation={1}>
+      <Link href='/'><MenuItem className={classes.button}>Home</MenuItem></Link>
+      <Link href='/Flights'><MenuItem className={classes.button}>Flights</MenuItem></Link>
+      <Link href='/Aircraft'><MenuItem className={classes.button}>Aircraft</MenuItem></Link>
+      <Link href='/Instructors'><MenuItem className={classes.button}>Instructors</MenuItem></Link>
+      <Link href='/billing'><MenuItem className={classes.button}>Billing</MenuItem></Link>
+      <Link href='/Settings'><MenuItem className={classes.button}>Settings</MenuItem></Link>
+      </MenuList>
+    </div>
+  );
 }
-export default Navigation;
+
+Navigation.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Navigation);
+
+{/* <MenuList>
+  <Link><MenuItem>Home</MenuItem></Link>
+  <Link><MenuItem>Flights</MenuItem></Link>
+  <Link><MenuItem>Aircraft</MenuItem></Link>
+  <Link><MenuItem>Instructors</MenuItem></Link>
+  <Link><MenuItem>Billing</MenuItem></Link>
+  <Link><MenuItem>Settings</MenuItem></Link>
+</MenuList> */}
+
+// const Navigation= ()=>{
+    //     return(
+        //         <div>
+        //             <ul>
+        //                 <li><Link href='/'><a>Home</a></Link></li>
+        //                 <li><Link href='/Flights'><a>Flights</a></Link></li>
+        //                 <li><Link href='/Aircraft'><a>Aircraft</a></Link></li>
+        //                 <li><Link href='/Instructors'><a>Instructors</a></Link></li>
+        //                 <li><Link href='/billing'><a>Billing</a></Link></li>
+        //                 <li><Link href='/Settings'><a>Settings</a></Link></li>
+        //             </ul>
+        //         </div>
+        //     )
+        // }
+        // export default Navigation;
--- frontend/pages/Flights.js
+++ frontend/pages/Flights.js
@@ -1,10 +1,121 @@
 import Layout from "../components/Layout";
-const FlightList = () => (
-  <Layout>
-    <div>
-      <p>Hello from flightList Next.js</p>
-    </div>
-  </Layout>
-);
 
-export default FlightList;
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+
+
+const styles = theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  fab: {
+    margin: theme.spacing.unit,
+    [theme.breakpoints.up(900 + theme.spacing.unit * 3 * 2)]: {
+      width: 75,
+      height: 75,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  AddIcon: {
+  },
+  Button: {
+    marginRight: "15px",
+  },
+  appBar: {
+    position: 'relative',
+  },
+  toolbarTitle: {
+    flex: 1,
+  },
+  layout: {
+    display: 'flex',
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(900 + theme.spacing.unit * 3 * 2)]: {
+      width: 900,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  heroContent: {
+    maxWidth: 600,
+    display: "flex",
+    justifyContent: 'center',
+    flexDirection: 'column',
+    margin: theme.spacing.unit * 14,
+    // marginTop: '120px',
+    padding: `${theme.spacing.unit * 0}px 0 ${theme.spacing.unit * 0}px`,
+  },
+  footer: {
+    marginTop: theme.spacing.unit * 8,
+    borderTop: `1px solid ${theme.palette.divider}`,
+    padding: `${theme.spacing.unit * 6}px 0`,
+  },
+});
+
+function Landing(props) {
+  const { classes } = props;
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="static" color="default" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+            Insert Name/Logo Here
+          </Typography>
+          <Button color="primary" variant="outlined">
+            Sign Out
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Layout>
+      <main className={classes.layout}>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+            Add New Flight
+          </Typography>
+        <Fab color="primary" aria-label="Add" className={classes.fab}>
+          <AddIcon />
+        </Fab>
+        </div>
+        {/* End hero unit */}
+      </main>
+      </Layout>
+      {/* Footer */}
+      <footer className={classNames(classes.footer, classes.layout)}>
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+  );
+}
+
+Landing.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Landing);
+
+// const FlightList = () => (
+//   <Layout>
+//     <div>
+//       <p>Hello from flightList Next.js</p>
+//     </div>
+//   </Layout>
+// );
+
+// export default FlightList;
