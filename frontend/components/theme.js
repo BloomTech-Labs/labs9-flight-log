d6cb500981fa5cde710e3e5efcee508fa1df7753
Trevor Lewis <tenzingtitan@gmail.com>
2019-01-10 17:25:53 -0700

    New Global UI theme, and adjustments for Layout for universal navbar

---
+++ frontend/components/theme.js
@@ -0,0 +1,21 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const palette = {
+      primary: {
+        light: '#e1eff7',
+        main: '#afbdc4',
+        dark: '#808d94',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#8bf6ff',
+        main: '#4fc3f7',
+        dark: '#0093c4',
+        contrastText: '#000',
+      },
+    };
+const themeName = 'FlightTheme'
+
+
+
+  export default createMuiTheme({ palette, themeName });
