8bb2f8bf81f928ca7d3f54b13ad3c29511152835
jeffyjkang <jeffyjkang@gmail.com>
2019-01-13 00:19:23 -0800

    update all routes, read me, etc

--- backend/routes/flightsRoute.js
+++ backend/routes/flightsRoute.js
@@ -18,11 +18,51 @@ router.post("/", async (req, res) => {
   if (!req.body.flightName) {
     return res.status(400).json({ error: "please input flight name" });
   }
+  if (!req.body.pilotID) {
+    return res.status(400).json({ error: "please input pilot id" });
+  }
   try {
     const flight = await flightsDb.insert(req.body);
     res.status(201).json(flight);
   } catch (error) {
     res.status(500).json({ error: "there was an error creating a flight" });
+  }
+});
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  if (!req.body.flightName) {
+    return res.status(400).json({ error: "please input flight name" });
+  }
+  if (!req.body.pilotID) {
+    return res.status(400).json({ error: "please input pilot id" });
+  }
+  try {
+    const flight = await flightsDb.update(id, req.body);
+    if (!flight) {
+      res
+        .status(400)
+        .json({ error: "the flight with the specified id does not exist" });
+    }
+    res.status(200).json(req.body);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "there was an error in updating the flight" });
+  }
+});
+//delete route
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const flight = await flightsDb.remove(id);
+    if (!flight) {
+      res
+        .status(404)
+        .json({ error: "the flight with the specified id does not exist" });
+    }
+    res.status(200).json({ message: "flight deleted" });
+  } catch (error) {
+    res.status(500).json({ error: "there was an error deleting the flight" });
   }
 });
 module.exports = router;
