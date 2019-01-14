40e8e1d86933a81dcc5fa094bb460df59799f1f0
ssmodish <smodish@hotmail.com>
2019-01-14 14:02:50 -0500

    edit knexfile.js to look for amazon db

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,31 +1,33 @@
 // Update with your config settings.
 require("dotenv").config();
 
-module.exports = {
-  development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./dev.sqlite3"
-    },
-    useNullAsDefault: true
-  }
-  // development: {
-  //   client: "mysql",
-  //   version: "5.6.41",
+const dbConnection = process.env.DATABASE_URL;
 
+module.exports = {
+  // development: {
+  //   client: "sqlite3",
   //   connection: {
-  //     host: process.env.HOST,
-  //     port: "3306",
-  //     user: process.env.USER,
-  //     password: process.env.PASS,
-  //     database: "labs9flightlog"
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
+  //     filename: "./dev.sqlite3"
   //   },
   //   useNullAsDefault: true
-  // },
+  // }
+  development: {
+    client: "mysql",
+    version: "5.6.41",
+
+    connection: {
+      host: process.env.HOST,
+      port: process.env.PORT,
+      user: process.env.USER,
+      password: process.env.PASS,
+      database: "labs9flightlog"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    useNullAsDefault: true
+  },
 
   // staging: {
   //   client: "postgresql",
