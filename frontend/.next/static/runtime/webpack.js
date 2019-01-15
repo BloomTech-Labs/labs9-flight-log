255f4bd14dbc78ce75924730ace0a8bc15f7f64d
jeffyjkang <jeffyjkang@gmail.com>
2019-01-14 17:33:58 -0800

    update front end to bring in data from local machine, readded cors

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "89d1055ab1073f1194b4";
+/******/ 	var hotCurrentHash = "f4e72d57f614d160656f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/pages/Instructors.js
+++ frontend/pages/Instructors.js
@@ -1,10 +1,34 @@
 import Layout from "../components/Layout";
-const InstructorList = () => (
-  <Layout>
-    <div>
-      <p>Hello from instructorList Next.js</p>
-    </div>
-  </Layout>
-);
+import React, { Component } from "react";
+import axios from "axios";
 
-export default InstructorList;
+// const InstructorList = () => (
+//   <Layout>
+//     <div>
+//       <p>Hello from instructorList Next.js</p>
+//     </div>
+//   </Layout>
+// );
+
+//import instructors, this component will allow users to perform crud operations on instructors
+//get all instructors refernced by user id/name, create, update, delete
+class InstructorsList extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  componentDidMount() {
+    axios.get("http://localhost:9000/instructors").then(response => {
+      console.table(response.data);
+    });
+  }
+  render() {
+    return (
+      <Layout>
+        <div>Hello from InstructorsList page</div>
+      </Layout>
+    );
+  }
+}
+
+export default InstructorsList;
