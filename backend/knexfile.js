36b7a070530549a0bf0d324dff558d1c3cd02bc0
jeffyjkang <jeffyjkang@gmail.com>
2019-01-17 08:21:15 -0800

    second update for knex file

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -13,23 +13,18 @@ module.exports = {
     useNullAsDefault: true
   },
 
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
-
   production: {
+    client: "mysql",
+    connection: {
+      host: process.env.HOST,
+      port: "3306",
+      user: process.env.USER,
+      password: process.env.PASS,
+      database: "labs9flightlog"
+    },
+    useNullAsDefault: true
+  },
+  staging: {
     client: "postgresql",
     connection: {
       database: "my_db",
