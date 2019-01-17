7d73742dea02d48fba58abefd317dd0a7411d7f8
ssmodish <smodish@hotmail.com>
2019-01-17 11:57:39 -0500

    cleaned knex file

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,4 +1,3 @@
-// Update with your config settings.
 require("dotenv").config();
 
 const dbConnection = process.env.DATABASE_URL;
@@ -16,19 +15,6 @@ module.exports = {
     useNullAsDefault: true
   },
   production: {
-    // client: "pg",
-    // connection: dbConnection, // can be and object or a string
-    // pool: {
-    //   min: 2,
-    //   max: 10
-    // },
-    // migrations: {
-    //   tableName: "knex_migrations",
-    //   directory: './migrations'
-    // },
-    // seeds: {
-    //   directory: './seeds'
-    // }
     client: "mysql",
     connection: {
       host: process.env.HOST,
