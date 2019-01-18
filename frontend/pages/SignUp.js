67ab576e58b557288f4d8080b84291751de96acc
Quan Nguyen <quan.nguyen199712@gmail.com>
2019-01-18 11:10:14 -0500

    added front end link to backend

--- frontend/pages/SignUp.js
+++ frontend/pages/SignUp.js
@@ -1,7 +1,8 @@
 import React from "react";
 import AuthenticationForm from "../components/AuthenticationForm";
-import fire from '../components/config/fire';
-import Router from 'next-router';
+import fire from "../components/config/fire";
+import Router from "next-router";
+import axios from 'axios';
 class SignUp extends React.Component {
   constructor(props) {
     super(props);
@@ -20,31 +21,46 @@ class SignUp extends React.Component {
       }
     });
   };
-  createUser= (event)=>{
-    fire.auth().createUserWithEmailAndPassword(this.state.user.username, this.state.user.password).then((result)=>{
-      console.log(result, 'hey created here')
-    }).catch((error)=>{
-      console.log(error, 'nope dude, youve gone goof')
-    })
+  createUser = event => {
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(
+        this.state.user.username,
+        this.state.user.password
+      )
+      .then(()=>{})
+      .catch(error => {
+        console.log(error, "nope dude, youve gone goof");
+      });
+  };
+  componentDidMount(){
+    fire.auth().currentUser.getIdToken(/* forceRefresh */ true).then(function(idToken) {
+      let headers={'token':idToken}
+      axios.post("https://labs9-flight-log.herokuapp.com/pilots", {headers:headers})
+      console.log(idToken)
+    }).catch(function(error) {
+      // Handle error
+      console.log(error)
+    });
   }
   render() {
     return (
       <div>
-         <div>
-      <form onSubmit={this.createUser}>
-        <input
-          name="username"
-          placeholder="Username"
-          onChange={this.handleChanges}
-        />
-         <input
-          name="password"
-          placeholder="password"
-          onChange={this.handleChanges}
-        />
-        <button onClick={this.createUser}>Sign Up</button>
-      </form>
-    </div>
+        <div>
+          <form onSubmit={this.createUser}>
+            <input
+              name="username"
+              placeholder="Username"
+              onChange={this.handleChanges}
+            />
+            <input
+              name="password"
+              placeholder="password"
+              onChange={this.handleChanges}
+            />
+            <button onClick={this.createUser}>Sign Up</button>
+          </form>
+        </div>
       </div>
     );
   }
