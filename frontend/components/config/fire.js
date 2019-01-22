232ca35e436012b5a067c8b2303596b6d53b59cd
jeffyjkang <jeffyjkang@gmail.com>
2019-01-21 16:05:10 -0800

    basic firebase storage dl and ul

--- frontend/components/config/fire.js
+++ frontend/components/config/fire.js
@@ -17,6 +17,19 @@ try {
     console.error("Firebase initialization error raised", err.stack);
   }
 }
-
 const fire = firebase;
 export default fire;
+
+// import firebase from "firebase/app";
+// import "firebase/storage";
+// const config = {
+//   apiKey: "AIzaSyCP-EjmMT_X7izYqnqN2UgGqJFpb-wOoKM",
+//   authDomain: "lab9-flightlogs.firebaseapp.com",
+//   databaseURL: "https://lab9-flightlogs.firebaseio.com",
+//   projectId: "lab9-flightlogs",
+//   storageBucket: "lab9-flightlogs.appspot.com",
+//   messagingSenderId: "491408929287"
+// };
+// firebase.initializeApp(config);
+// const storage = firebase.storage();
+// export { storage, firebase as default };
