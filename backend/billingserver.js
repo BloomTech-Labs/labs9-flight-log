34bf9a276f5625c37ad380e44e6783210619534c
Wanda <doubleuevans@gmail.com>
2019-01-24 18:27:55 -0600

    billing bug fix

--- backend/billingserver.js
+++ backend/billingserver.js
@@ -3,32 +3,49 @@ require("dotenv").config();
 const app = require("express")();
 const stripe = require("stripe")(process.env.SAPIKEY);
 const cors = require('cors')
+const express = require('express');
 
 const port = process.env.PORT || 8000;
 
 app.use(cors());
-app.use(require("body-parser").text());
+//app.use(require("body-parser").text());
+app.use(express.json());
 
-server.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("<h1>Billing Server running</h1>");
 });
 
 app.post("/charge", async (req, res) => {
-  console.log(req.body)
-  try {
-    let {status} = await stripe.charges.create({
-      amount: 2000,
-      currency: "usd",
-      description: "An example charge",
-      source: req.body
-    });
-
-    res.json({status});
-  } catch (err) {
-    console.log(err);
-    res.status(500).end();
+  console.log('@@@BILLING SERVER@@@', req.body.token)
+  console.log('appPost-req.amount', req.body.amount)
+  if(req.body.amount === "1999") {
+    try {
+      let {status} = await stripe.charges.create({
+        amount: 1999,
+        currency: "usd",
+        description: "yearly purchase",
+        source: req.body.token
+      });
+      res.json({status});
+    } catch (err) {
+      console.log(err);
+      res.status(500).end();
+    }    
+  } else {
+    try {
+      let {status} = await stripe.charges.create({
+        amount: 999,
+        currency: "usd",
+        description: "monthly purchase",
+        source: req.body.token
+      });
+      res.json({status});
+    } catch (err) {
+      console.log(err);
+      res.status(500).end();
+    }    
   }
-});
+});//app.post end brackets
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
--- react-frontend/src/components/Module Components/billings/BillingForm.js
+++ react-frontend/src/components/Module Components/billings/BillingForm.js
@@ -1,38 +1,77 @@
 import React, {Component} from 'react';
 import {CardElement, injectStripe} from 'react-stripe-elements';
+import axios from 'axios';
 
-class CheckoutForm extends Component {
+class BillingForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {complete: false}
-    this.submit = this.submit.bind(this);
+    this.state = {complete: false, value:""}
+    this.submit = this.submit.bind(this)
+
   }
 
+  setAmount = (ev) => {
+    console.log('setAmount', ev.target.value);
+    this.setState({value: ev.target.value})
+  }
+
+  //https://labs9-flight-log.herokuapp.com
+  //http://localhost:8000
+  
   async submit(ev) {
     // User clicked submit
+    
+/*     
+    let body = {
+      //amount:this.state.value, 
+      token: await this.props.stripe.createToken({name: "Name"})
+      }
+    
+    await axios
+      .post('http://localhost:8000/charge', body)
+      .then(res => {
+        console.log("SUCCESS")
+      })
+      .catch(error => console.log(error)) */
+      
     let {token} = await this.props.stripe.createToken({name: "Name"});
+    let amount = await this.state.value
+
     let response = await fetch("https://labs9-flight-log.herokuapp.com/charge", {
       method: "POST",
-      headers: {"Content-Type": "text/plain"},
-      body: token.id
+      headers: {"Content-Type": "application/json"},
+      //application/json
+      //text/plain
+      body: JSON.stringify({token:token.id, amount})
+      //amount: amount
     });
 
     if (response.ok) this.setState({complete:true})
     console.log("Purchase Complete!")
+
   }
 
   render() {
     if (this.state.complete) return <h1>Purchase Complete!</h1>;
 
     return (
-      <div className="checkout">
-        <p>Would you like to complete the purchase?</p>
+      <div className="checkout">     
         <CardElement />
+        <p>Please select your preferred subscription:</p>
+        <div onChange={this.setAmount}>
+          <input type="radio" id="year" 
+                  name="sub" value="1999"/>
+          <label htmlFor="1999">$19.99 for 1 year</label>
+
+          <input type="radio" id="month" 
+                  name="sub" value="999"/>
+          <label htmlFor="999">$9.99 for 1 month</label>        
+        </div>
         <button onClick={this.submit}>Send</button>
       </div>
     );
   }
 }
 
-export default injectStripe(CheckoutForm);
+export default injectStripe(BillingForm);
 //export default injectStripe(CheckoutForm);
