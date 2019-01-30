a2b496e3b36ab4b9404d887529514151335a0344
Wanda <doubleuevans@gmail.com>
2019-01-30 16:00:54 -0600

    materialUI upgrades: new radiobutton HOC, button component in billingform

--- react-frontend/src/components/Module Components/billings/BillingForm.js
+++ react-frontend/src/components/Module Components/billings/BillingForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { CardElement, injectStripe } from "react-stripe-elements";
+import Button from "@material-ui/core/Button";
+import RadioButtonsGroup from "./RadioButtonsGroup"
 import axios from "axios";
 
 class BillingForm extends Component {
@@ -33,11 +35,13 @@ class BillingForm extends Component {
       this.setState({ complete: true});
       const UID = localStorage.getItem('userID');
       console.log(UID);
+      //console.log('isPaid status:', {isPaid});
 
       axios
         .post(`https://labs9-flight-log.herokuapp.com/pilots/${UID}`)
         .then(response => {
           this.setState({ isPaid: true})
+          //console.log('isPaid post status:', {isPaid})
         })
         .catch(error => console.log(error));
         console.log("Purchase Complete!");
@@ -52,17 +56,28 @@ class BillingForm extends Component {
       <div className="checkout">
         <CardElement />
         <p>Please select your preferred subscription:</p>
-        <div onChange={this.setAmount}>
-          <input type="radio" id="year" name="sub" value="1999" />
-          <label htmlFor="1999">$19.99 for 1 year</label>
 
-          <input type="radio" id="month" name="sub" value="999" />
-          <label htmlFor="999">$9.99 for 1 month</label>
-        </div>
-        <button onClick={this.submit}>Send</button>
+        <RadioButtonsGroup/>
+
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={this.submit}
+        >
+          Buy Now
+        </Button>
+
       </div>
     );
   }
 }
 
 export default injectStripe(BillingForm);
+
+/* <div onChange={this.setAmount}>
+<input type="radio" id="year" name="sub" value="1999" />
+<label htmlFor="1999">$19.99 for 1 year</label>
+
+<input type="radio" id="month" name="sub" value="999" />
+<label htmlFor="999">$9.99 for 1 month</label>
+</div> */
