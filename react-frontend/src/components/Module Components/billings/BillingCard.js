9a9bf87e97838771db56ea205c0a199386ec95b3
Wanda <doubleuevans@gmail.com>
2019-01-31 12:25:02 -0600

    billing

---
+++ react-frontend/src/components/Module Components/billings/BillingCard.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { CardElement, /* injectStripe */ } from "react-stripe-elements";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+const styles = {
+  card: {
+    maxWidth: 300,
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 8,
+  },
+};
+
+function BillingCard(props) {
+  const { classes } = props;
+  //const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <CardElement />
+      </CardContent>
+    </Card>
+  );
+}
+
+BillingCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(BillingCard);
