a8e4d9875fe82e5a62331c72bdf4d02d9276a41d
Smodi <stevesmodish@gmail.com>
2019-02-05 17:59:32 -0500

    Adjusted imports for build

--- react-frontend/src/components/Pages/Instructors.js
+++ react-frontend/src/components/Pages/Instructors.js
@@ -4,7 +4,6 @@ import axios from "axios";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import { withStyles } from "@material-ui/core/styles";
 
